0e6ffe0b0b7abb02a7b44dce19ace1e63fee5933
Damian Kucharski <kuchar187@gmail.com>
2022-09-01 22:03:31 +0200

    Add repositories interfaces

---
+++ src/Services/Restaurant/Restaurant.Application/Contracts/ICuisineTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Restaurant.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Application.Contracts
+{
+    public interface ICuisineTypeRepository
+    {
+        Task<IEnumerable<CuisineTypeEntity>> GetCuisineTypesByRestaurantId(string restaurantId);
+        Task AddCuisineTypesToRestaurant(string restaurantId, int[] cuisineTypeIds);
+        Task RemoveCuisineTypesFromRestaurant(string restaurantId, int[] cuisineTypeIds);
+    }
+}
