8b32d7790ec044b64fc3f2e939953900f99a6724
Damian Kucharski <kuchar187@gmail.com>
2022-09-10 20:11:18 +0200

    config persistence

---
+++ src/Services/Restaurant/Restaurant.Persistence/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Persistence.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection RegisterPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<RestaurantDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("RestaurantConnectionString")
+            ));
+
+            return services;
+        }
+    }
+}
---
+++ src/Services/Restaurant/Restaurant.Persistence/RestaurantDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant.Domain.Common;
+using Restaurant.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Persistence
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RestaurantEntity> Restaurants { get; set; }
+        public DbSet<CuisineTypeEntity> RestaurantCuisineTypes { get; set; }
+        public DbSet<DishEntity> Dishes { get; set; }
+        public DbSet<DishTypeEntity> DishTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(RestaurantDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach(var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                entry.Entity.LastDateModified = DateTime.Now;
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Entity.ExternalId = Guid.NewGuid();
+                    entry.Entity.DateCreated = DateTime.Now;
+                }   
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
